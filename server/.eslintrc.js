c9b3ce3ef6d438bf9da996634b94af962b026754
Ioannis Lafiotis <giannislafiotis@Gianniss-MBP.fritz.box>
2020-04-09 15:28:26 +0200

    added the server for the project

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/essential"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  plugins: ["vue"],
+  rules: {},
+};
